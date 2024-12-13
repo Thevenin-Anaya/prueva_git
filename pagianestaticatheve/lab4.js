faff359822c48af2bcd55c7141201cf6036aebee
Thevenin-Anaya <t28jav@gmail.com>
2024-12-13 09:22:14 -0500

    Add files via upload

---
+++ pagianestaticatheve/lab4.js
@@ -0,0 +1,5 @@
+document.getElementById('ContacForm').addEventListener('submit',function(event){
+    event.preventDefault();//evitar que se recarge la pagina//
+    alert('Formulario enviado correctamente gracias por contactarnos..!')
+
+});
